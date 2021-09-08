7e032b98b5c9dde041a73dabd824e979a4f78730
erianerik <erianerik@gmail.com>
2021-09-08 11:14:41 -0300

    chore: Responsividade para menu mobile

---
+++ js/menu.js
@@ -0,0 +1,21 @@
+let menuIcon = $(".menu-mobile");
+let menuContent = $(".nav__list-content");
+let closeMenu = $(".close-menu");
+
+function scrollActive(status){
+    let propertie = status ? "hidden" : "initial";
+    return $("html").css("overflow", propertie);
+}
+
+menuIcon.click(() => {
+    scrollActive(true);
+    closeMenu.fadeIn();
+    menuContent.fadeIn().css("display", "flex");
+});
+
+closeMenu.click(() => {
+    scrollActive(false);
+    closeMenu.fadeOut();
+    menuContent.fadeOut();
+});
+

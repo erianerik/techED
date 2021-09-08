bbdb1e1358af2cd27330d8bff1584674a248cfc1
erianerik <erianerik@gmail.com>
2021-09-08 12:31:22 -0300

    chore: ajuste import keyframe

--- js/splash.js
+++ js/splash.js
@@ -2,7 +2,7 @@ function startSplash() {
     let timeSplash = Math.floor(Math.random() * (1500 - 1150) + 1150);
 
     $("html").css("overflow", "hidden");
-    
+
     setTimeout(function(){
         $(".splash").fadeOut();
         $("html").css("overflow", "initial");
